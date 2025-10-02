3ce182a66cd6febb748db861d97e92f439889379
MargaritaOliv <231108@ids.upchiapas.edu.mx>
2025-10-02 08:57:41 -0600

    listo

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const morgan = require('morgan')
 const piratasRoutes = require('./src/routes/piratas-routes.js')
-const { handleError } = require('./src/middlewares/error-middleware.js')
 
 app.use(express.json())
 app.use(cors())
 app.use(morgan('dev'))
-app.use(piratasRoutes)
-app.use(handleError)
+app.use('/api', piratasRoutes)
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
-})  
-require('dotenv').config()
+})
--- src/routes/piratas-routes.js
+++ src/routes/piratas-routes.js
@@ -1,6 +1,7 @@
-const { getPiratas, getPirataById, createPirata, updatePirata, deletePirata } = require ('../controllers/piratas-controller')
+const { getAllPiratas, getPirataById, createPirata, deletePirata } = require ('../controllers/piratas-controller')
 const router = require('express').Router();
-router.get('/piratas', getPiratas)
+
+router.get('/piratas', getAllPiratas)  
 router.get('/piratas/:id', getPirataById)
 router.post('/piratas', createPirata)
 router.delete('/piratas/:id', deletePirata)
