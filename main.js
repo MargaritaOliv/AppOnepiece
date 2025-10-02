a30e2144517fec67ce838259b4cd542e75d8fab3
MargaritaOliv <231108@ids.upchiapas.edu.mx>
2025-10-02 09:06:51 -0600

    fkfjf

--- main.js
+++ main.js
@@ -5,13 +5,15 @@ const app = express()
 const cors = require('cors')
 const morgan = require('morgan')
 const piratasRoutes = require('./src/routes/piratas-routes.js')
+const Connection = require('./src/config/db.js')
 
 app.use(express.json())
 app.use(cors())
 app.use(morgan('dev'))
 app.use('/api', piratasRoutes)
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT
 app.listen(PORT, () => {
+    Connection.getConnection()
     console.log(`Server is running on port ${PORT}`)
 })
